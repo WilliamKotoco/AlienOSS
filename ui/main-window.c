a4e14eae22525bf4c11e78d71717af122b1808f7
William Martins <williamdabruzzmartins76136@gmail.com>
2024-05-04 21:08:56 -0300

    Adding process state window and creating responsivity

--- ui/main-window.c
+++ ui/main-window.c
@@ -6,7 +6,9 @@
 void show_and_run() {
 
   WINDOW *option_window;
-
+  /// secondary windows
+  WINDOW *process_state_window;
+  WINDOW *memory_state_window;
   initscr();
   cbreak();
 
@@ -15,9 +17,10 @@ void show_and_run() {
   init_pair(2, COLOR_GREEN, COLOR_BLACK);
   bkgd(COLOR_PAIR(1));
 
-  refresh();
-
-  option_window = newwin(100, 50, 0, 0);
+  load_process_window(&process_state_window);
+  //  load_memory_window(memory_state_window);
+  /// get the maximum from the standard screen
+  option_window = newwin(getmaxy(stdscr), getmaxx(stdscr) / 4, 0, 0);
 
   /// colloring the window
   wbkgd(option_window, COLOR_PAIR(1));
@@ -61,7 +64,8 @@ void show_and_run() {
       /// Enter key
     case '\n':
       if (highlight == 0) {
-        mvwprintw(option_window, 35, 1, "Created"); /// temporary
+        mvwprintw(process_state_window, 10, 10, "Created"); /// temporary
+        wrefresh(process_state_window);
 
       } else if (highlight == 1) {
         mvwprintw(option_window, 35, 1, "Toggled"); /// temporary
@@ -77,4 +81,19 @@ void show_and_run() {
   wrefresh(option_window);
 
   endwin();
+}
+
+void load_process_window(WINDOW **win) {
+  *win = newwin(getmaxy(stdscr) / 2, getmaxx(stdscr) * (3 / 4), 0,
+                getmaxx(stdscr) * 1 / 4);
+
+  wbkgd(*win, COLOR_PAIR(1));
+  wattron(*win, COLOR_PAIR(2));
+
+  box(*win, 0, 0);
+
+  wattron(*win, A_BOLD);
+  mvwprintw(*win, 0, 2, "Process window");
+  wattroff(*win, A_BOLD);
+  wrefresh(*win);
 }
--- ui/main-window.h
+++ ui/main-window.h
@@ -10,3 +10,5 @@
 /// Display the logo and the options to create a process, display memory and
 /// display idk
 void show_and_run();
+
+void load_process_window(WINDOW **);
